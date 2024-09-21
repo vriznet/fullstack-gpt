070af6d54808f8a37cc123d5a78d3c4242fe95bd
vriznet <vriz@vriz.net>
2024-09-21 15:39:55 +0900

    7.4 Chat Messages - 2

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -8,19 +8,24 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
-with st.chat_message("human"):
-    st.write("Hello, DocumentGPT!")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-with st.chat_message("ai"):
-    st.write("Hello, human!")
 
-st.chat_input("Send a message to the ai")
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state.messages.append({"message": message, "role": role})
 
-with st.status("Embedding file...", expanded=True) as status:
+
+for message in st.session_state.messages:
+    send_message(message["message"], message["role"], save=False)
+
+
+message = st.chat_input("Send a message to the ai")
+
+if message:
+    send_message(message, "human")
     time.sleep(2)
-    st.write("Getting the file")
-    time.sleep(2)
-    st.write("Embedding the file")
-    time.sleep(2)
-    st.write("Caching the file")
-    status.update(label="Error", state="error")
+    send_message(f"You said: {message}", "ai")
