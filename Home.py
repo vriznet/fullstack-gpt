e9de4c2495be9fe0e8915c39e102334891501846
vriznet <vriz@vriz.net>
2024-09-21 11:48:32 +0900

    7.3 Multi Page - 1

--- Home.py
+++ Home.py
@@ -1,11 +1,18 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("title")
 
-st.subheader("Welcome to Streamlit")
+with st.sidebar:
+    st.title("sidebar title")
+    st.text_input("text input")
 
-st.markdown(
-    """
-  #### I love it!
-"""
-)
+tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
+
+with tab_one:
+    st.write("a")
+
+with tab_two:
+    st.write("b")
+
+with tab_three:
+    st.write("c")
