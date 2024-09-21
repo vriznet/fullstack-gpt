a671928424a3fb8242644a5f32d3e32bdd47d5f5
vriznet <vriz@vriz.net>
2024-09-21 14:12:27 +0900

    7.4 Chat Messages - 1

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,26 @@
+import time
 import streamlit as st
 
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
+
 st.title("DocumentGPT")
+
+with st.chat_message("human"):
+    st.write("Hello, DocumentGPT!")
+
+with st.chat_message("ai"):
+    st.write("Hello, human!")
+
+st.chat_input("Send a message to the ai")
+
+with st.status("Embedding file...", expanded=True) as status:
+    time.sleep(2)
+    st.write("Getting the file")
+    time.sleep(2)
+    st.write("Embedding the file")
+    time.sleep(2)
+    st.write("Caching the file")
+    status.update(label="Error", state="error")
