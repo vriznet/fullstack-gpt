90a9ddcb458da15d49ebcc32dfcc37977692f7c9
vriznet <vriz@vriz.net>
2024-09-22 21:55:46 +0900

    7.7 Chat History

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -12,10 +12,11 @@ st.set_page_config(
 )
 
 
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
-    st.write(file_content, file_path)
+
     with open(file_path, "wb") as f:
         f.write(file_content)
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
@@ -35,6 +36,22 @@ def embed_file(file):
     return retriever
 
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
 st.title("DocumentGPT")
 
 st.markdown(
@@ -42,15 +59,22 @@ st.markdown(
 Welcome!
 
 Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
-
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("Winston")
-    st.write(s)
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+else:
+    st.session_state["messages"] = []
