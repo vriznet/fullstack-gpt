4af318a016ad3a8fa9b389d5fb03812894666a89
vriznet <vriz@vriz.net>
2024-09-30 11:05:26 +0900

    9.1 WikipediaRetriever

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,52 @@
 import streamlit as st
+from langchain_community.retrievers import WikipediaRetriever
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="🤔",
+)
 
 st.title("QuizGPT")
+
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx, .txt or .pdf file", type=["docx", "txt", "pdf"]
+        )
+        if file:
+            docs = split_file(file)
+            st.write(docs)
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5, lang="ko")
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
+                st.write(docs)
