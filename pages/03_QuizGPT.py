c85e8cf61fb917b1e72ba4512ddbf1ab7bc77276
vriznet <vriz@vriz.net>
2024-10-07 10:19:52 +0900

    9.4 Formatter Prompt

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -24,12 +24,17 @@ def split_file(file):
     return docs
 
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
 st.set_page_config(
     page_title="QuizGPT",
     page_icon="🤔",
 )
 
 st.title("QuizGPT")
+
 
 llm = ChatOpenAI(
     model="gpt-3.5-turbo",
@@ -39,9 +44,164 @@ llm = ChatOpenAI(
 )
 
 
-def format_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a helpful assistant that is role playing as a teacher.
+            Based ONLY on the following context make 10 questions to test
+            the user's knowledge about the text.
+            Each question should have 4 answers, three of them must be
+            incorrect and one should be correct.
+            Use (o) to signal the correct answer.
 
+            Question examples:
+
+            Question: What is the color of the ocean?
+            Answers: Red|Yellow|Green|Blue(o)
+
+            Question: What is the capital or Georgia?
+            Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+            Question: When was Avatar released?
+            Answers: 2007|2001|2009(o)|1998
+
+            Question: Who was Julius Caesar?
+            Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+            Your turn!
+
+            Context: {context}
+            """,
+        )
+    ]
+)
+
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+                You are a powerful formatting algorithm.
+
+                You format exam questions into JSON format.
+                Answers with (o) are the correct ones.
+
+                Example Input:
+                Question: What is the color of the ocean?
+                Answers: Red|Yellow|Green|Blue(o)
+
+                Question: What is the capital or Georgia?
+                Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+                Question: When was Avatar released?
+                Answers: 2007|2001|2009(o)|1998
+
+                Question: Who was Julius Caesar?
+                Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+                Example Output:
+
+                ```json
+                {{ "questions": [
+                        {{
+                            "question": "What is the color of the ocean?",
+                            "answers": [
+                                    {{
+                                        "answer": "Red",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Yellow",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Green",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Blue",
+                                        "correct": true
+                                    }},
+                            ]
+                        }},
+                                    {{
+                            "question": "What is the capital or Georgia?",
+                            "answers": [
+                                    {{
+                                        "answer": "Baku",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Tbilisi",
+                                        "correct": true
+                                    }},
+                                    {{
+                                        "answer": "Manila",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Beirut",
+                                        "correct": false
+                                    }},
+                            ]
+                        }},
+                                    {{
+                            "question": "When was Avatar released?",
+                            "answers": [
+                                    {{
+                                        "answer": "2007",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "2001",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "2009",
+                                        "correct": true
+                                    }},
+                                    {{
+                                        "answer": "1998",
+                                        "correct": false
+                                    }},
+                            ]
+                        }},
+                        {{
+                            "question": "Who was Julius Caesar?",
+                            "answers": [
+                                    {{
+                                        "answer": "A Roman Emperor",
+                                        "correct": true
+                                    }},
+                                    {{
+                                        "answer": "Painter",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Actor",
+                                        "correct": false
+                                    }},
+                                    {{
+                                        "answer": "Model",
+                                        "correct": false
+                                    }},
+                            ]
+                        }}
+                    ]
+                }}
+                ```
+                Your turn!
+                Questions: {context}
+            """,
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
 
 with st.sidebar:
     docs = None
@@ -77,43 +237,11 @@ if not docs:
     """
     )
 else:
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
-                You are a helpful assistant that is role playing as a teacher.
-                Based ONLY on the following context make 10 questions to test
-                the user's knowledge about the text.
-                Each question should have 4 answers, three of them must be
-                incorrect and one should be correct.
-                Use (o) to signal the correct answer.
-
-                Question examples:
-
-                Question: What is the color of the ocean?
-                Answers: Red|Yellow|Green|Blue(o)
-
-                Question: What is the capital or Georgia?
-                Answers: Baku|Tbilisi(o)|Manila|Beirut
-
-                Question: When was Avatar released?
-                Answers: 2007|2001|2009(o)|1998
-
-                Question: Who was Julius Caesar?
-                Answers: A Roman Emperor(o)|Painter|Actor|Model
-
-                Your turn!
-
-                Context: {context}
-                """,
-            )
-        ]
-    )
-
-    chain = {"context": format_docs} | prompt | llm
-
     start = st.button("Generate Quiz")
 
     if start:
-        chain.invoke(docs)
+        questions_response = questions_chain.invoke(docs)
+        formatting_response = formatting_chain.invoke(
+            {"context": questions_response.content}
+        )
+        st.write(formatting_response.content)
