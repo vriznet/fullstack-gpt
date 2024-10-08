87da737da3c05a48dd95caff6ff316bfbfc8c4d9
vriznet <vriz@vriz.net>
2024-10-08 12:15:54 +0900

    10.1 AsyncChromiumLoader

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,32 @@
 import streamlit as st
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain_community.document_transformers import Html2TextTransformer
+
+html2text_transformer = Html2TextTransformer()
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🌐",
+)
 
 st.title("SiteGPT")
+
+st.markdown(
+    """
+  Ask questions about the content of a website.
+
+  Start by writing the URL of the website on the sidebar.
+"""
+)
+
+with st.sidebar:
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+    )
+
+if url:
+    loader = AsyncChromiumLoader([url])
+    docs = loader.load()
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(transformed)
